8907d58554d8493e70899748aabd2b366438640c
rcd-systems <comptepoubelle8@gmail.com>
2017-02-03 23:35:05 +0100

    Table nav

--- src/main/resources/assets/js/rcd-jsfwk-material-tables.js
+++ src/main/resources/assets/js/rcd-jsfwk-material-tables.js
@@ -202,7 +202,7 @@ class RcdMaterialTableNav extends RcdDivElement {
     }
 
     generateIteratorText() {
-        return (parseInt(this.start) + 1) + ' - ' + (parseInt(this.start) + this.count) + ' of ' + this.total;
+        return (parseInt(this.start) + 1) + ' - ' + (parseInt(this.start) + Math.max(1, this.count)) + ' of ' + this.total;
     }
 
     setValues(start, count, total) {
