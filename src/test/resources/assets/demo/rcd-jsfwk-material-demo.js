b4b0d2840b3ed67faa9817bc169a27afe9d0d6f4
rcd-systems <comptepoubelle8@gmail.com>
2017-03-25 11:50:07 +0100

    2.0 refactoring

--- src/test/resources/assets/demo/rcd-jsfwk-material-demo.js
+++ src/test/resources/assets/demo/rcd-jsfwk-material-demo.js
@@ -4,51 +4,70 @@ function createApp() {
         const breadCrumbs = new RcdMaterialBreadcrumbs().init().
             addBreadcrumb(new RcdMaterialBreadcrumb('Demo', () => RcdHistoryRouter.getInstance().setState()).init()).
             addBreadcrumb(new RcdMaterialBreadcrumb('Layout').init());
-        const layout= new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init());
+        const layout = new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init());
         return {
             state: 'layout',
             name: 'Layout',
             iconArea: new RcdGoogleMaterialIconArea('open_in_new').init(),
-            callback: (main) => main.addChild(breadCrumbs).addChild(layout)
-        };
-    }
+            callback: (main) => main.addChild(breadCrumbs).addChild(layout).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init())).
+                addChild(new RcdMaterialLayout().init().addChild(new RcdTextElement('Test').init()))
+    };
+}
 
-    function createDialogsRoute() {
-        const breadCrumbs = new RcdMaterialBreadcrumbs().init().
-            addBreadcrumb(new RcdMaterialBreadcrumb('Demo', () => RcdHistoryRouter.getInstance().setState()).init()).
-            addBreadcrumb(new RcdMaterialBreadcrumb('Dialogs').init());
-        
-        return {
-            state: 'dialogs',
-            name: 'Dialogs',
-            iconArea: new RcdGoogleMaterialIconArea('open_in_new').init(),
-            callback: (main) => main.addChild(breadCrumbs)
-        };
-    }
+function createDialogsRoute() {
+    const breadCrumbs = new RcdMaterialBreadcrumbs().init().
+        addBreadcrumb(new RcdMaterialBreadcrumb('Demo', () => RcdHistoryRouter.getInstance().setState()).init()).
+        addBreadcrumb(new RcdMaterialBreadcrumb('Dialogs').init());
 
-    function createDefaultRoute() {
-        const view = new RcdTextElement('Default').init();
-        return {
-            callback: (main) => main.addChild(view)
-        };
-    }
+    return {
+        state: 'dialogs',
+        name: 'Dialogs',
+        iconArea: new RcdGoogleMaterialIconArea('open_in_new').init(),
+        callback: (main) => main.addChild(breadCrumbs)
+    };
+}
 
-    function createTablesRoute() {
-        const view = new RcdTextElement('Tables').init();
-        return {
-            state: 'tables',
-            name: 'Tables',
-            iconArea: new RcdGoogleMaterialIconArea('grid_on').init(),
-            callback: (main) => main.addChild(view)
-        };
-    }
+function createTablesRoute() {
+    const view = new RcdTextElement('Tables').init();
+    return {
+        state: 'tables',
+        name: 'Tables',
+        iconArea: new RcdGoogleMaterialIconArea('grid_on').init(),
+        callback: (main) => main.addChild(view)
+    };
+}
 
-    return new RcdMaterialSinglePageApplication('Demo').
-        init().
-        setDefaultRoute(createDefaultRoute()).
-        addRoute(createLayoutRoute()).
-        addRoute(createDialogsRoute()).
-        addRoute(createTablesRoute());
+function createDefaultRoute() {
+    const view = new RcdTextElement('Default').init();
+    return {
+        callback: (main) => main.addChild(view)
+    };
+}
+
+return new RcdMaterialSinglePageApplication('Demo').
+    init().
+    setDefaultRoute(createDefaultRoute()).
+    addRoute(createLayoutRoute()).
+    addRoute(createDialogsRoute()).
+    addRoute(createTablesRoute());
 }
 
 createApp().start(document.body);
